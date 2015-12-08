cf8e325d69a76b890e5cdd1d01c9fba6c6cca001
skp1002515 <bruce.shin@sk.com>
2015-12-08 18:20:12 +0900

    change tab into space

--- src/main/scala/memento/Memento.scala
+++ src/main/scala/memento/Memento.scala
@@ -5,75 +5,75 @@ import java.util.Random
 
 
 trait Originator {
-	type T <: Memento
-	def createMemento:T 
-	def setMemento(m:T)
-	trait Memento {
-		def getState[Originator]
-		def setState[Originator]
-	}
+  type T <: Memento
+  def createMemento:T 
+  def setMemento(m:T)
+  trait Memento {
+    def getState[Originator]
+    def setState[Originator]
+  }
 }
 
 class Gamer(var money:Int) extends Originator {
-	type T = GameMemento
+  type T = GameMemento
 
-	val random = new Random
-	var fruits = new ListBuffer[String]
-	val fruitsName = Array("사과","포도","바나나","귤")
+  val random = new Random
+  var fruits = new ListBuffer[String]
+  val fruitsName = Array("사과","포도","바나나","귤")
 
-	class GameMemento extends Memento{
-		var m:Int = _
-		var f = new ListBuffer[String]
-		def getMoney = m
+  class GameMemento extends Memento{
+    var m:Int = _
+    var f = new ListBuffer[String]
+    def getMoney = m
 
-		/**
-		 * TODO : private 으로, How?
-		 **/
-		def getState[Originator] = {
-			money = this.m
-			fruits = this.f
-		}
-			
-		/**
-		 * TODO : private 으로, How?
-		 **/
-		def setState[Originator] = { 
-			this.m = money
-			fruits.filter(_.startsWith("맛있다")).map(this.f += _)
-		}
-	}
+    /**
+     * TODO : private 으로, How?
+     **/
+    def getState[Originator] = {
+      money = this.m
+      fruits = this.f
+    }
+      
+    /**
+     * TODO : private 으로, How?
+     **/
+    def setState[Originator] = { 
+      this.m = money
+      fruits.filter(_.startsWith("맛있다")).map(this.f += _)
+    }
+  }
 
-	def createMemento:GameMemento = {
-		val m = new GameMemento()
-		m.setState
-		m
-	}
+  def createMemento:GameMemento = {
+    val m = new GameMemento()
+    m.setState
+    m
+  }
 
-	def setMemento(m:GameMemento) = m.getState
+  def setMemento(m:GameMemento) = m.getState
 
-	def bet() {
-		/**
-		 * TODO : 각 조건에 따른 로직을 Lamda로 ??
-		 **/
-		val dice = random.nextInt(6) + 1		
-		if(dice == 1){
-			money += 100
-			println("돈이 증가했습니다.")
-		}else if(dice == 2){
-			money /= 2
-			println("돈이 반으로 줄어들었습니다.")
-		}else if(dice == 6){
-			val fruit = getFruit()
-			println(s"과일($fruit)을 받았습니다.")
-			fruits += fruit	
-		}else{
-			println("아무일도 일어나지 않았습니다.")
-		}
-	}
+  def bet() {
+    /**
+     * TODO : 각 조건에 따른 로직을 Lamda로 ??
+     **/
+    val dice = random.nextInt(6) + 1    
+    if(dice == 1){
+      money += 100
+      println("돈이 증가했습니다.")
+    }else if(dice == 2){
+      money /= 2
+      println("돈이 반으로 줄어들었습니다.")
+    }else if(dice == 6){
+      val fruit = getFruit()
+      println(s"과일($fruit)을 받았습니다.")
+      fruits += fruit 
+    }else{
+      println("아무일도 일어나지 않았습니다.")
+    }
+  }
 
-	def getFruit() : String = {
-		val prefix = if(random.nextBoolean) "맛있다" else ""
-		prefix + fruitsName(random.nextInt(fruitsName.length))
-	}
-	override def toString:String = s"[money = $money , fruits = $fruits ]"
+  def getFruit() : String = {
+    val prefix = if(random.nextBoolean) "맛있다" else ""
+    prefix + fruitsName(random.nextInt(fruitsName.length))
+  }
+  override def toString:String = s"[money = $money , fruits = $fruits ]"
 }
--- src/main/scala/memento/MementoApp.scala
+++ src/main/scala/memento/MementoApp.scala
@@ -3,28 +3,28 @@ package memento
 import memento.game._
 
 object MementoApp extends App{
-	
-	val gamer = new Gamer(100)
-	var memento = gamer.createMemento
+  
+  val gamer = new Gamer(100)
+  var memento = gamer.createMemento
 
-	for(i <- 0 until 30){
+  for(i <- 0 until 30){
 
-		println(s"==== $i")
-		println(s"현 상태 $gamer")
+    println(s"==== $i")
+    println(s"현 상태 $gamer")
 
-		gamer.bet
+    gamer.bet
 
-		println(s"돈은 ${gamer.money} 원이 되었습니다.")
+    println(s"돈은 ${gamer.money} 원이 되었습니다.")
 
-		if(gamer.money > memento.getMoney){
-			println("  (많이 증가했으니, 현재의 상태를 보존해두자)")
-			memento = gamer.createMemento
-			println(memento.getState)
-		}else if(gamer.money < memento.getMoney / 2){
-			println("  (많이 줄었으니 이전의 상태로 복귀하자)")
-			gamer.setMemento(memento)
-		}
+    if(gamer.money > memento.getMoney){
+      println("  (많이 증가했으니, 현재의 상태를 보존해두자)")
+      memento = gamer.createMemento
+      println(memento.getState)
+    }else if(gamer.money < memento.getMoney / 2){
+      println("  (많이 줄었으니 이전의 상태로 복귀하자)")
+      gamer.setMemento(memento)
+    }
 
-		Thread sleep 1000
-	}
+    Thread sleep 1000
+  }
 }
