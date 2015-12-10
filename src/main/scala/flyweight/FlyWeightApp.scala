2b7d6c0d23777d3bdea952610d4a6a656d534e77
skp1002515 <bruce.shin@sk.com>
2015-12-10 13:41:26 +0900

    change tab into 2 space

--- src/main/scala/flyweight/FlyWeightApp.scala
+++ src/main/scala/flyweight/FlyWeightApp.scala
@@ -1,14 +1,14 @@
 package flyweight
 
 object FlyWeightApp{
-	
-	def main(args:Array[String]){
+  
+  def main(args:Array[String]){
 
-		if(args == null || args.length == 0){
-			println("Usage: sbt \"run-main flyweight.FlyWeightApp digit\"")
-			println("Example: sbt \"run-main flyweight.FlyWeightApp 123123\"")
-			System.exit(0)
-		}
-		new BigString(args(0)).print()
-	}	
+    if(args == null || args.length == 0){
+      println("Usage: sbt \"run-main flyweight.FlyWeightApp digit\"")
+      println("Example: sbt \"run-main flyweight.FlyWeightApp 123123\"")
+      System.exit(0)
+    }
+    new BigString(args(0)).print()
+  } 
 }
